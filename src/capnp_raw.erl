a2e09a509f87d1de9c8f586cab41dc176e7a4b7a
David Buckley <bucko@gambitresearch.com>
2015-03-31 11:07:45 +0100

    capnp_raw -- jam tracebacks into the tree if we crash; makes testing easier

--- src/capnp_raw.erl
+++ src/capnp_raw.erl
@@ -58,16 +58,16 @@ decode_pointer(<<0:64/little-integer>>, _) ->
 decode_pointer(P= <<DataOffsetLSB:6/little-integer, 0:2/little-integer, DataOffsetMSB:24/little-integer, DataSize:16/little-integer, PointerSize:16/little-integer>>, Message=#message{current_offset=CurrentOffset}) ->
 	DataOffset = DataOffsetLSB + (DataOffsetMSB bsl 6),
 	%io:format("Struct pointer: ~p, ~p~n", [P, {CurrentOffset+1+DataOffset, DataSize, PointerSize}]),
-	decode_struct(DataSize, PointerSize, Message#message{current_offset=CurrentOffset+1+DataOffset});
+	(catch decode_struct(DataSize, PointerSize, Message#message{current_offset=CurrentOffset+1+DataOffset}));
 decode_pointer(P= <<DataOffsetLSB:6/little-integer, 1:2/little-integer, DataOffsetMSB:24/little-integer, ListLengthLSB:5/little-integer, ElementSize:3/little-integer, ListLengthMSB:24/little-integer>>, Message=#message{current_offset=CurrentOffset}) ->
 	DataOffset = DataOffsetLSB + (DataOffsetMSB bsl 6),
 	ListLength = ListLengthLSB + (ListLengthMSB bsl 5),
 	%io:format("List pointer: ~p, ~p~n", [P, {{CurrentOffset, DataOffsetLSB, DataOffsetMSB, CurrentOffset+1+DataOffset}, {ListLength, ListLengthLSB, ListLengthMSB}}]),
-	decode_list(ElementSize, ListLength, Message#message{current_offset=CurrentOffset+1+DataOffset});
+	(catch decode_list(ElementSize, ListLength, Message#message{current_offset=CurrentOffset+1+DataOffset}));
 decode_pointer(P= <<SegmentOffsetLSB:5/little-integer, LandingPadExtra:1/little-integer, 2:2/little-integer, SegmentOffsetMSB:24/little-integer, SegmentNumber:32/little-integer>>, Message) ->
 	SegmentOffset = SegmentOffsetLSB + (SegmentOffsetMSB bsl 5),
 	%io:format("Far pointer: ~p, ~p~n", [P, {{SegmentOffset, SegmentOffsetLSB, SegmentOffsetMSB}, SegmentNumber, LandingPadExtra}]),
-	decode_far_pointer(SegmentOffset, SegmentNumber, LandingPadExtra, Message);
+	(catch decode_far_pointer(SegmentOffset, SegmentNumber, LandingPadExtra, Message));
 decode_pointer(P= <<0:6/little-integer, 3:2/little-integer, 0:24/little-integer, CapabilityOffset:32/little-integer>>, #message{}) ->
 	%io:format("Capability: ~p, ~p~n", [P, CapabilityOffset]),
 	{not_implemented_capabilities, CapabilityOffset};
@@ -83,7 +83,7 @@ decode_struct(DataWords, PointerWords, Message=#message{current_segment=CS, curr
 	%io:format("Data: ~p; Pointers: ~p~n", [Data, Pointers]),
 	#struct{
 		data=Data,
-		pointers=decode_pointers(Pointers, Message#message{current_offset=Offset+DataWords})
+		pointers=(catch decode_pointers(Pointers, Message#message{current_offset=Offset+DataWords}))
 	}.
 
 decode_list(6, Length, Message=#message{current_segment=CS, current_offset=Offset}) ->
@@ -92,22 +92,23 @@ decode_list(6, Length, Message=#message{current_segment=CS, current_offset=Offse
 	<<_:OffsetSize/binary, Pointers:PointerSize/binary, _/binary>> = CS,
 	%io:format("List pointers: ~p~n", [Pointers]),
 	#list{
-		data=decode_pointers(Pointers, Message#message{current_offset=Offset+1})
+		data=(catch decode_pointers(Pointers, Message#message{current_offset=Offset+1}))
 	};
 decode_list(7, _WordLength, Message=#message{current_segment=CS, current_offset=Offset}) ->
 	OffsetSize = Offset * 8,
-	<<_:OffsetSize/binary, ListLengthLSB:6/little-integer, 0:2/little-integer, ListLengthMSB:24/little-integer, DataWords:16/little-integer, PointerWords:16/little-integer, _/binary>> = CS,
+	<<_:OffsetSize/binary, CS1/binary>> = CS,
+	<<ListLengthLSB:6/little-integer, 0:2/little-integer, ListLengthMSB:24/little-integer, DataWords:16/little-integer, PointerWords:16/little-integer, _/binary>> = CS1,
 	ListLength = ListLengthLSB + (ListLengthMSB bsl 6),
 	%io:format("Struct list.~n"),
 	#list{
-		data=[ decode_struct(DataWords, PointerWords, Message#message{current_offset=Offset+1+I*(DataWords+PointerWords)}) || I <- lists:seq(0, ListLength-1) ]
+		data=[ (catch decode_struct(DataWords, PointerWords, Message#message{current_offset=Offset+1+I*(DataWords+PointerWords)})) || I <- lists:seq(0, ListLength-1) ]
 	};
 decode_list(Size, Length, #message{current_segment=CS, current_offset=Offset}) ->
 	BitSize = element(Size+1, {0, 1, 8, 16, 32, 64}),
 	OffsetSize = Offset * 8,
 	DataSize = BitSize * Length,
 	<<_:OffsetSize/binary, Data:DataSize/bitstring, _/binary>> = CS,
-	Elts = decode_list_elements(BitSize, Length, Data),
+	Elts = (catch decode_list_elements(BitSize, Length, Data)),
 	%io:format("Element list: ~p~n", [Elts]),
 	#list{data=Elts}.
 
@@ -121,7 +122,7 @@ decode_list_elements(S, L, Data) ->
 decode_far_pointer(SegmentOffset, SegmentNumber, 0, Message=#message{segments=Segments}) ->
 	NewSegment = element(SegmentNumber+1, Segments),
 	%io:format("Far pointer ~p (~p)~n", [SegmentOffset, SegmentNumber]),
-	decode_pointer(Message#message{current_segment=NewSegment, current_offset=SegmentOffset}).
+	(catch decode_pointer(Message#message{current_segment=NewSegment, current_offset=SegmentOffset})).
 
 test() ->
 	{ok, Data} = file:read_file("capnp.raw"),
